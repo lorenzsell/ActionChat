e4753b953b4e83f824b6b9956020d83b487caacf
Lorenz Sell <lorenz@sutra.co>
2017-05-25 14:42:36 -0400

    chat hacking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_cable.allowed_request_origins = ['https://frozen-bastion-16804.herokuapp.com',
-  #                                                'http://frozen-bastion-16804.herokuapp.com']
-  #
-  # config.action_cable.url = "wss://frozen-bastion-16804.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://frozen-bastion-16804.herokuapp.com',
+                                                 'http://frozen-bastion-16804.herokuapp.com']
+
+  config.action_cable.url = "wss://frozen-bastion-16804.herokuapp.com/cable"
 end
